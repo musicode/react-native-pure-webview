da784b0d6c6f6a211c8452f877a5ff938bac559d
musicode <musicode@qq.com>
2018-11-05 15:07:27 +0800

    update

--- index.js
+++ index.js
@@ -3,14 +3,7 @@ import React, {
   Component,
 } from 'react'
 
-import {
-  WebView,
-  Platform,
-} from 'react-native'
-
-import WKWebView from 'react-native-wkwebview-reborn'
-
-const isIos = Platform.OS === 'ios'
+import { WebView } from 'react-native-webview'
 
 const script = `
 window.sendMessage = function (str) {
@@ -41,8 +34,6 @@ sendMessage({
 // 最后这句 sendMessage 是为 Ios 准备的
 // 因为 Ios 不会触发 onLoad ...
 // 因此机制变成了双重保证，即 onLoad 或 webview 谁先触发 load 就谁赢了
-
-const WebViewComponent = isIos ? WKWebView : WebView
 
 export default class PureWebView extends Component {
 
@@ -105,7 +96,7 @@ export default class PureWebView extends Component {
       injectedJavaScript = script
     }
     return (
-      <WebViewComponent
+      <WebView
         {...props}
         ref="webview"
         injectedJavaScript={injectedJavaScript}
